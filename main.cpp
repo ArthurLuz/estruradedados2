20051191d2a4839e5d37bc72e7473fcc59718c07
ArthurLuz <arthurluz17@uft.edu.br>
2016-06-15 23:09:01 -0300

    hue

--- main.cpp
+++ main.cpp
@@ -4,26 +4,27 @@
 int main(int argc, char **argv){
 
     Ordenacao *x;
-    int vetor[20];
+    int vetor[10];
     x = new Ordenacao(vetor);
 
     srand((unsigned int)time(NULL));
 
-    for(int i=0;i<20;i++){ //preenche o vetor
+    for(int i=0;i<10;i++){ //preenche o vetor
 		    vetor[i] = 1 + ( rand() % 100 );//rand()%100000+1;
     }
     int inicio=0;
-    int fim=19;
-    int tam =20;
-    for(int i=0;i<20;i++){
+    int fim=9;
+    int tam =10;
+
+    for(int i=0;i<tam;i++){
       cout << "original: "<< vetor[i] << endl; //mostra o vetor antes de arrumar
     }
-   // x->mergeSort(vetor,inicio,fim);
+   //x->mergeSort(vetor,inicio,fim);
    // x->selectionsort(vetor);
    // x->bublesort(vetor);
     //x->insertionSort(vetor);
-    x->quickSort(vetor,inicio,fim);
-        for(int i=0;i<20;i++){
+    x->quicksort(vetor,inicio,fim,0);
+    for(int i=0;i<tam;i++){
       cout << "ordenado:  "<< vetor[i] << endl; //mostra depois de ordenado
     }
 
@@ -33,9 +34,5 @@ int main(int argc, char **argv){
 
 
 
-<<<<<<< HEAD
-}//}
 
-=======
 }
->>>>>>> 83b0a0a4a99050d673747f00681b265728d4cabc
--- ordenacao.hpp
+++ ordenacao.hpp
@@ -58,53 +58,94 @@ class Ordenacao{
       }
   };
 //_________________________________________________________________________________________
-  void mergeSort(int *Vetor, int posicaoInicio, int posicaoFim) {
-    int i, j, k, metadeTamanho,*vetorTemp;
 
+  void mergeSort(int *vetor, int posicaoInicio, int posicaoFim) {
+    int i, j, k, metadeTamanho, *vetorTemp,aux;
 
     if(posicaoInicio == posicaoFim) return;
 
     // ordenacao recursiva das duas metades
     metadeTamanho = (posicaoInicio + posicaoFim ) / 2;
-    mergeSort(Vetor, posicaoInicio, metadeTamanho);
-    mergeSort(Vetor, metadeTamanho + 1, posicaoFim);
+    mergeSort(vetor, posicaoInicio, metadeTamanho);
+    mergeSort(vetor, metadeTamanho + 1, posicaoFim);
 
     // intercalacao no vetor temporario t
     i = posicaoInicio;
     j = metadeTamanho + 1;
     k = 0;
-    int v [posicaoFim - posicaoInicio + 1];
-    vetorTemp = v ;
+     vetorTemp = new int [posicaoFim - posicaoInicio + 1];
 
     while(i < metadeTamanho + 1 || j  < posicaoFim + 1) {
         if (i == metadeTamanho + 1 ) { // i passou do final da primeira metade, pegar v[j]
-            vetorTemp[k] = Vetor[j];
+            vetorTemp[k] = vetor[j];
             j++;
             k++;
-        }else {
+        }
+        else {
             if (j == posicaoFim + 1) { // j passou do final da segunda metade, pegar v[i]
-                vetorTemp[k] = Vetor[i];
+                vetorTemp[k] = vetor[i];
                 i++;
                 k++;
-            }else {
-                if (Vetor[i] < Vetor[j]) {
-                    vetorTemp[k] = Vetor[i];
+            }
+            else {
+                if (vetor[i] < vetor[j]) {
+                    vetorTemp[k] = vetor[i];
                     i++;
                     k++;
-                }else {
-                    vetorTemp[k] = Vetor[j];
+                } else {
+                    vetorTemp[k] = vetor[j];
                     j++;
                     k++;
                 }
             }
         }
+
     }
     // copia vetor intercalado para o vetor original
     for(i = posicaoInicio; i <= posicaoFim; i++) {
-        Vetor[i] = vetorTemp[i - posicaoInicio];
+        vetor[i] = vetorTemp[i - posicaoInicio];
     }
     free(vetorTemp);
 };
+// void mergeSort(int *vetor,int inicio,int fim) {
+//
+//   if (fim <= inicio) {
+// 		return;
+// 	}
+// 	int meio = (inicio + fim) / 2;
+// 	meio++;
+// 	//mergeSort();
+//   int *A =new int [meio - inicio + 1];
+// 	int *B= new int [fim - meio];
+// 	for (int i = 0; i <= meio - inicio; i++) {
+// 		A[i] = vetor[inicio + i];
+// 	}
+// 	for (int i = 0; i <= fim - meio - 1; i++) {
+// 		B[i] = vetor[meio + 1 + i];
+// 	}
+//   mergeSort(A,meio,meio-inicio+1);
+//   mergeSort(B,inicio,fim-meio);
+// 	int i = 0;
+// 	int j = 0;
+// 	for (int k = inicio; k <= fim; k++) {
+// 		if (i < meio - inicio + 1 && j < fim - meio) {
+// 			if (A[i] < B[j]) {
+// 				vetor[k] = A[i++];
+// 			} else {
+// 				vetor[k] = B[j++];
+// 			}
+// 		} else if (i < meio - inicio + 1) {
+// 			vetor[k] = A[i++];
+// 		} else if (j < fim - meio) {
+// 			vetor[k] = B[j++];
+// 		}
+// 	}
+// };
+
+
+
+
+
     void ShellSort( int *vetor,int tamanho) {
      int i, temp;
      bool b = false;
@@ -123,59 +164,55 @@ class Ordenacao{
      }
      return;
 };
-<<<<<<< HEAD
-void quickSort(int valor[], int esquerda, int direita){
-    int i, j;
-    i = esquerda;
-    j = direita;
-    x = valor[(esquerda + direita) / 2];
 
-    while(i <= j){
-        while(valor[i] < x && i < direita){
-            i++;
-        }
-        while(valor[j] > x && j > esquerda){
-            j--;
-        }
-        if(i <= j){
-            z = valor[i];
-            valor[i] = valor[j];
-            valor[j] = z;
-            i++;
-            j--;
-        }
-    }
-    if(j > esquerda){
-        quickSort(valor, esquerda, j);
-    }
-    if(i < direita){
-        quickSort(valor,  i, direita);
-    }
-};
-    /*void quicksort(int *vetor,int tam){
+
+
+    void quicksort(int *vetor,int inicio,int fim,int aboll){
+      int indexpivo;
+      int auxboll = aboll;
+      if(auxboll<=0)indexpivo=inicio;
+      else indexpivo=fim;
         int aux;
-        for (int i=0;i<tam;i++){
-            aux =vetor[i];
-            for (int j=tam;j>i;j--){
-                if (aux>vetor[j]){
-                    aux = vetor[j];
-                    vetor[j]=vetor[i];
-                    vetor[i]=aux;
-                }
-            }
-=======
-    void quicksort(int *vetor,int inicio,int fim){
-        int indexpivo;
         if (inicio<fim){
           // encontra o index pivo
-          quicksort(vetor,inicio,indexpivo-1);
-          quicksort(vetor,indexpivo+1,fmi);
->>>>>>> 83b0a0a4a99050d673747f00681b265728d4cabc
-        }
+
+            //indexpivo=inicio;
+            //do{
+            if(vetor[indexpivo]>vetor[fim]){
+              aux =vetor[indexpivo];
+              vetor[indexpivo]=vetor[fim];
+              vetor[fim]=aux;
+              //indexpivo=inicio;
+              auxboll=1;
+              std::cout << "/* message */" << std::endl;
+            }else{
+              fim--;
+            }
+          //}while(fim>inicio || auxboll!=1);
+          //do{
+            if(vetor[indexpivo]<vetor[inicio]){
+              aux =vetor[indexpivo];
+              vetor[indexpivo]=vetor[inicio];
+              vetor[inicio]=aux;
+              //indexpivo=fim;
+              auxboll=-1;
+            }else{
+                  inicio++;
+            }
+          //}while(inicio<fim||auxboll!=-1);
+
+          }
+          //indexpivo=encontrapivo(vetor,inicio,fim,auxboll);
+          quicksort(vetor,indexpivo+1,fim,auxboll);
+          quicksort(vetor,inicio,indexpivo-1,auxboll);
+
+        };
+        // int encontrapivo(int *vetor,int inicio,int fim, int auxboll){
+        //   int i = inicio;
+        //   int f = fim;
+        //   int aux= auxboll;
+        //     if(aux=1) return f;
+        //     if (aux=-1) return i;
+        // };
 
     };
-*/
-
-
-};
-
